893c47747db78fde28fdea0f7193800100e6fdf1
vrkljam <vrkljam@earthlink.net>
2022-11-17 16:25:29 -0700

    started js code for functionality

---
+++ main.js
@@ -0,0 +1,15 @@
+const videoElement =document.getElementById('video');
+const button =document.getElementById('button');
+
+// select media stream, pass video to element then play
+
+async function selectMediaStream(){
+    try {
+
+    }
+    catch (error){
+       console.log('oops', error)
+    }
+}
+
+selectMediaStream ();
